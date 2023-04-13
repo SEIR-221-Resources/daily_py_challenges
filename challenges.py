7cab5394f5c986b02da79b70ad2b4909580f48b0
nprati01 <nicole.e.prati@gmail.com>
2023-04-13 16:07:18 -0400

    please explain 28 i have confusion

--- challenges.py
+++ challenges.py
@@ -198,7 +198,19 @@ print(f'two_sum solution: \n > {two_sum(nums, target)} = [0, 1]')
 # Your solution for 28-roman_to_integer here:
 
 def roman_to_int(s):
-    pass
+    numerals ={"I": 1, "V": 5, "X":10, "L":50, "D": 500, "M":1000, 'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+    output=0
+    i = 0
+    while i < len(s):
+        if i+1<len(s) and s[i:i+2]in numerals:
+            output+=numerals[s[i:i+2]]
+            i+=2
+            print(output)
+        else:
+            print(i)
+            output+= numerals[s[i]]
+            i+=1
+    return output
 
-r='LVIII'
+r='IV'
 print(f'roman_to_int solution: \n > {roman_to_int(r)} = 58')
