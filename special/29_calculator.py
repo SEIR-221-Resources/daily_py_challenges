6e546aed69b4831528023f7c361867a2d31f8594
AnthonyScott <anthony.scott1005@gmail.com>
2023-04-16 11:02:32 -0400

    Add solution for 29

--- special/29_calculator.py
+++ special/29_calculator.py
@@ -20,4 +20,26 @@
 # what they typed in to a string.
 
 # Your solution for 29-calculator here:
+def add(a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mult(a,b):
+    return a * b
+def div(a,b):
+    return a / b
 
+prompt = input('What calculation would you like to do? (add, sub, mult, div): ').upper()
+num1 = int(input('What is the first number? '))
+num2 = int(input('What is the second number? '))
+
+if prompt == 'ADD':
+    print(f"Solution: {add(num1,num2)}")
+elif prompt == 'SUB':
+    print(f"Solution: {sub(num1,num2)}")
+elif prompt == 'MULT':
+    print(f"Solution: {mult(num1,num2)}")
+elif prompt == 'DIV':
+    print(f"Solution: {div(num1,num2)}")
+else:
+    print("Please enter a valid computation operation")
